df2b42405756450a3b3826ab7e170b4403e27788
Al3 <72630590+PlayerBuuzz@users.noreply.github.com>
2026-01-26 21:13:40 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,18 +2,25 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJECT.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyD6a29L24aFSgVjmC7NFGXpdWW4g1uQsio",
+  authDomain: "tocomfome-754e1.firebaseapp.com",
+  projectId: "tocomfome-754e1",
+  storageBucket: "tocomfome-754e1.firebasestorage.app",
+  messagingSenderId: "997975886937",
+  appId: "1:997975886937:web:9bf408c35f1ed8aea13548",
+  measurementId: "G-PYJME2PXRD"
 });
 
 const messaging = firebase.messaging();
 
+// 🔔 Notificações recebidas em background
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
+  console.log("Mensagem recebida em background:", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
     icon: "/icon.png"
-  });
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
